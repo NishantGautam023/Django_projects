8fd0057bd7c1171ec74d841195d93639d297ce3c
Nishant <eddienishant@gmail.com>
2020-05-05 14:57:23 +0530

    Added templates and path

--- Ecomerce/ecommerce/store/views.py
+++ Ecomerce/ecommerce/store/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# creating a function based view
+
+
+def store(request):
+    context = {}  # creating a context dictionary where we will pass some data, making empty for now
+    return render([request], ['store/store.html'], [context])
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+

156bb669a2dad30bfca68fa03aecb0976ad6ec02
Nishant <eddienishant@gmail.com>
2020-05-06 09:59:59 +0530

    Module 6 completed

--- mfdw/pages/views.py
+++ mfdw/pages/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1> Welcome Nishant </h1>")
+    return render(request, 'base.html')
 # Create your views here.
